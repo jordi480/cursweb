bfb1021a0b40bf020ce833549a8e9e38cb3dbd4a
curs2018 <curs2018@BI101-04.epsevg.upc.es>
2018-02-23 13:55:05 +0100

    up

--- WEB/JQUERY/js/exercici-j1.js
+++ WEB/JQUERY/js/exercici-j1.js
@@ -1,6 +1,23 @@
 
-$(document).ready(function(){
-    $(this).click(function(){
-        $("#pantalla").load("pag1.html");
+ $(document).ready(function(){
+    $("input").on("click",function(){
+        
+        var link=$(this).attr("src");
+        console.log(link);
+        $("#pantalla").load(link);
      });
-});
+}); 
+
+/*$(document).ready(function(){
+    $("input").on("click",function(event){
+        event.preventDefault();
+        var link=$(this).attr("href");
+        $("#pantalla").load(link);
+             ,function(response,status,xhr){
+            if ( status == "error" ) {
+                var msg = "Sorry but there was an error: ";
+                $( "#pantalla" ).html( msg + xhr.status + " " + xhr.statusText );
+            }
+            $("#pantalla").html=response; 
+        });*/
+ 
---
+++ WEB/JQUERY/js/exercici-j2.js
@@ -0,0 +1,10 @@
+
+
+
+$(document).ready(function(){
+    $("input").on("click",function(){
+            $("#formulari").load("html/opció1.html #carrer");
+     });
+}); 
+
+
