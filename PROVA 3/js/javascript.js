35cf95cd10263c409b2b29e002699168552d7a49
curs2018 <curs2018@BI101-04.epsevg.upc.es>
2018-03-07 13:44:49 +0100

    up

---
+++ PROVA 3/js/javascript.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(function(){
+
+    $("#contacte").on("click",function(){
+        
+       $("#contingut").load("html/formulari.html #container");
+        
+
+        
+    });
+
+    $("#geo").on("click",function(){
+        
+        $("#contingut").load("html/localitzacio.html");
+      
+ 
+         
+     });
+     
+            
+      
+ 
+         
+     });
+
+
+     
+
+
+
+
+
+
+
